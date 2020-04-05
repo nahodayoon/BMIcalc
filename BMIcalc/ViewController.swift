98bfd8a31d2d42dae685489f1720ba1accf97544
nahoneisanma <nahoneisanma@icloud.com>
2020-04-05 12:48:20 +0900

    Initial Commit

---
+++ BMIcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMIcalc
+//
+//  Created by Naho Otani on 2020/04/05.
+//  Copyright © 2020 Naho Otani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
